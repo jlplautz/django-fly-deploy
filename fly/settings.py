6b50f9e3456d5ab2fc91e7f4eb57fe0a1689771c
Jorge Luiz Plautz <jorge.plautz@gmail.com>
2023-03-24 19:37:36 -0300

    6-corrected_settings_CI_CD_V54

--- fly/settings.py
+++ fly/settings.py
@@ -25,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool, default=False)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default='127.0.0.1')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
 
 # Application definition
 
