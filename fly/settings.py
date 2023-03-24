3ff952143c9c32e30df5452d76c5fe9b4011cf69
Jorge Luiz Plautz <jorge.plautz@gmail.com>
2023-03-24 01:07:59 -0300

    4-created_github_actions_CI_CD_V3

--- fly/settings.py
+++ fly/settings.py
@@ -10,8 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
-import os
-
 from pathlib import Path
 from decouple import config, Csv
 from dj_database_url import parse as db_url
@@ -124,7 +122,8 @@ USE_TZ = True
 
 STATIC_ROOT = BASE_DIR / 'public'
 STATIC_URL = 'static/'
-# STATICFILES_STORE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
